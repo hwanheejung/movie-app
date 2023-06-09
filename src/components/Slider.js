a437391aa78bea3bdd92f1c780a9b22fec0fc317
hwanheejung <hwanheejung117@gmail.com>
2023-06-09 21:57:07 +0900

    updated home slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,11 +6,43 @@ import SliderMovies from "./SliderMovies";
 function Slider({ API_byGroup }) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [moved, setMoved] = useState(0);
+  const [cnt, setCnt] = useState(1);
+  const [rArrow, setrArrow] = useState("block");
+  const [lArrow, setlArrow] = useState("none");
+  console.log("curr cnt:", cnt);
 
   const getMovies = async () => {
     const json = await (await fetch(API_byGroup)).json();
     setMovies(json.data.movies);
     setLoading(false);
+  };
+  const paintArrow = (cnt, lHide, rHide) => {
+    if (cnt === lHide) {
+      setlArrow("none");
+      setrArrow("block");
+    } else if (cnt === rHide) {
+      setrArrow("none");
+      setlArrow("block");
+    } else {
+      setrArrow("block");
+      setlArrow("block");
+    }
+  };
+
+  const handleRight = () => {
+    if (cnt <= 3) {
+      setCnt((curr) => (curr += 1));
+      setMoved((curr) => curr - 460 - 5 * cnt);
+      paintArrow(cnt, 0, 3);
+    }
+  };
+  const handleLeft = () => {
+    if (cnt > 1) {
+      setCnt((curr) => (curr -= 1));
+      setMoved((curr) => curr + 460 + 5 * (cnt - 1));
+      paintArrow(cnt, 2, 5);
+    }
   };
 
   useEffect(() => {
@@ -24,7 +56,10 @@ function Slider({ API_byGroup }) {
         {loading ? (
           <Loading />
         ) : (
-          <div className={styles.sliderWrapper}>
+          <div
+            className={styles.sliderWrapper}
+            style={{ transform: `translateX(${moved}px)` }}
+          >
             {movies.map((movie) => (
               <SliderMovies
                 key={movie.id}
@@ -37,6 +72,20 @@ function Slider({ API_byGroup }) {
             ))}
           </div>
         )}
+      </div>
+      <div
+        className={styles.rightArrow}
+        onClick={handleRight}
+        style={{ display: `${rArrow}` }}
+      >
+        ⟩
+      </div>
+      <div
+        className={styles.leftArrow}
+        onClick={handleLeft}
+        style={{ display: `${lArrow}` }}
+      >
+        ⟨
       </div>
     </div>
   );
